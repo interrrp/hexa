aee99e13fb65ba7b98fc25c7b5039c4c3f5ef87d
interrrp <int@addictedto.coffee>
2024-02-04 14:28:04 +0800

    Use `0x` prefix by default

--- src/script.js
+++ src/script.js
@@ -1,20 +1,27 @@
 import $ from "cash-dom";
 import "normalize.css";
 
-$("#input").on("input", () => {
-  const input = $("#input").val();
-  const hex = toHex(input, true);
-  $("#output").val(hex);
-});
+$("#input").on("input", update);
+$("#use-slash-x").on("change", update);
+
+function update() {
+  const inputValue = $("#input").val();
+  const useSlashX = $("#use-slash-x").is(":checked");
+  const hexString = toHex(inputValue, useSlashX);
+  $("#output").val(hexString);
+}
 
 /**
  * Convert a string to a hexadecimal string
  * @param {string} str The string to convert
- * @param {number} withX Whether to include `\x`
+ * @param {number} useSlashX Whether to use `\x` over `0x`
  * @returns {string} The hexadecimal string
  */
-function toHex(str, withX) {
+function toHex(str, useSlashX) {
   return Array.from(str)
-    .map((c) => (withX ? "\\x" : "") + c.charCodeAt(0).toString(16))
+    .map((char) => {
+      const hex = char.charCodeAt(0).toString(16);
+      return useSlashX ? `\\x${hex}` : `0x${hex} `;
+    })
     .join("");
 }
